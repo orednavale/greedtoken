8bb31861c6b575570d0806aacc176fd46dbda3af
Elmer Lavandero <elmerlavandero@gmail.com>
2017-11-11 01:20:38 +0800

    sixth commit

--- js/timer.js
+++ js/timer.js
@@ -1,6 +1,18 @@
 
 // Set the date we're counting down to
 
+/// utility methods
+const toEthString = wei => parseFloat((wei / 10**18).toFixed(4));
+
+/// constants
+const abi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"EXCHANGE_RATE","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"vestSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"icoWallet","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"icoFinished","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"icoSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"endICO","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalTokensSold","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_subtractedValue","type":"uint256"}],"name":"decreaseApproval","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"icoOpen","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"durationSeconds","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"bonusMultiplier","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"vestContract","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalRaised","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_addedValue","type":"uint256"}],"name":"increaseApproval","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"startTimestamp","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalContributors","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}];  
+const targetApi = 'https://mainnet.infura.io/';
+const contractAddress = '0xbc9395973bd35a3b4bd924f050d2778c07506ecb';
+
+/// getting contract
+var web3 = new Web3(new Web3.providers.HttpProvider(targetApi));
+var GreedTokenIco = web3.eth.contract(abi).at(contractAddress);
+
 var startDate = new Date("Nov 11, 2017 11:11:11 UTC");
 
 var numSaleDays = 1000;
@@ -10,7 +22,7 @@ var BonusDays3 = 3;
 var BonusDays4= 4;
 var BonusDays5 = 5;
 
-var endDate = (addMinutes(startDate, numSaleDays)).getTime();
+var endDate = new Date("Dec 12, 2017 12:12:12 UTC");
 
 var firstBonusEndDate = (addMinutes(startDate, BonusDays1)).getTime();
 var secondBonusEndDate = (addMinutes(startDate, BonusDays2)).getTime();
@@ -44,7 +56,8 @@ var x = setInterval(function() {
 		document.getElementById("tiles").innerHTML = "<span>" + tmlf[0] + "</span><span>" +  tmlf[1] + "</span><span>" + tmlf[2] + "</span><span>" + tmlf[3] + "</span>"; 
 		document.getElementById("cd-title").innerHTML = "<strong>WORLD'S BIGGEST ICO BONUS (1000X) STARTS IN</strong><br><br>";
 		document.getElementById("contribButt").innerHTML = contribButton2;
-	} else if (endDate > now) {
+		document.getElementById("stats").innerHTML = "";
+	} else if (endDate > now && !GreedTokenIco.icoFinished()) {
 		    document.getElementById("contrib").innerHTML = contribButton1;
 			document.getElementById("contribButt").innerHTML = contribButton3;
 			distance = endDate - now;
@@ -52,14 +65,17 @@ var x = setInterval(function() {
 			endingText = endText + tmlf[0] + " Days " + tmlf[1] + " Hours " + tmlf[2] + " Mins " + tmlf[3] + " Secs";
 			document.getElementById("timer").innerHTML = "<strong>" + endingText + "</strong>";
 			document.getElementById("tiles").innerHTML = "<span>" + tmlf[0] + "</span><span>" +  tmlf[1] + "</span><span>" + tmlf[2] + "</span><span>" + tmlf[3] + "</span>"; 
-			document.getElementById("cd-title").innerHTML = "<strong>WORLD'S BIGGEST ICO BONUS ENDS IN</strong><br><br>"
+			document.getElementById("cd-title").innerHTML = "<strong>WORLD'S BIGGEST ICO BONUS IS NOW LIVE</strong><br><br>";
+			document.getElementById("stats").innerHTML = '<h5 align="center"><strong>Current Bonus :</strong>  ' + toEthString(GreedTokenIco.bonusMultiplier()) + 'x&emsp;<strong>Total Tokens Sold :</strong> ' + toEthString(GreedTokenIco.totalTokensSold()) + '&emsp;<strong>Total ETHs Raised :</strong> ' + toEthString(GreedTokenIco.totalRaised()) + '<br><br></h5>';
+
 	} else {
 			clearInterval(x);
 			document.getElementById("contrib").innerHTML = '';
 			document.getElementById("contribButt").innerHTML = '';
 			document.getElementById("timer").innerHTML = "<strong>" + finishText + "</strong>";
 			document.getElementById("tiles").innerHTML = "<span>0</span><span>0</span><span>0</span><span>0</span>";
-			document.getElementById("cd-title").innerHTML = "<strong>TOKEN SALE IS FINISHED. THANK YOU CONTRIBUTORS!</strong><br><br>" 
+			document.getElementById("cd-title").innerHTML = "<strong>TOKEN SALE IS FINISHED. THANK YOU CONTRIBUTORS!</strong><br><br>";
+			document.getElementById("stats").innerHTML = '<h5 align="center"><strong>Last Bonus :</strong>  ' + toEthString(GreedTokenIco.bonusMultiplier()) + 'x&emsp;<strong>Total GREEDs Sold :</strong> ' + toEthString(GreedTokenIco.totalTokensSold()) + '&emsp;<strong>Total ETHs Raised</strong> : ' + toEthString(GreedTokenIco.totalRaised()) + '<br><br></h5>';
 	}
 	
     // Output the result in an element with id="demo"
